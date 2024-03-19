dba50f21dcb61cc14b0d06973ef30f2705bff36c
MilosJovic23 <milos.mj992@gmail.com>
2024-03-19 17:24:44 +0100

    * initial commit

---
+++ objekti.js
@@ -0,0 +1,16 @@
+var korpa = {
+	SSD: 500,
+	"Maticna ploca": 400,
+};
+
+
+
+console.log(korpa);
+korpa.GPU = 100;
+korpa.CDrom = 200;
+korpa.FloppyDisk = 900;
+console.log(korpa);
+korpa["Ram memorija"] = 100;
+korpa["HDD"] = 150;
+korpa["Procesori"] = 600;
+console.log(korpa);
---
+++ script.js
@@ -0,0 +1,11 @@
+//indexi  0         1             2            3
+
+
+
+var proizvodi = ["Maticna ploca", "Ram memorija", "SSD"];
+console.log(proizvodi);
+proizvodi[3] = "HDD";
+proizvodi.push("GPU", "Floppy disk", "CD rom");
+console.log(proizvodi);
+proizvodi[2] = "Procesori";
+console.log(proizvodi);
